c30727b0f566ca4ded6d43995724971034823cb3
NiniKo <pcninik@gmail.com>
2026-07-23 18:36:26 +0200

    Update dependencies

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
 from anime_sama_api.cli.downloader import download, multi_download
-from anime_sama_api.cli.episode_extra_info import convert_with_extra_info
 from anime_sama_api.episode import Episode, Languages, Players
 
 
@@ -11,17 +10,15 @@ def test_multi_download():
 
 def test_download():
     download(
-        convert_with_extra_info(
-            Episode(
-                Languages(
-                    vf=Players(),
-                    vostfr=Players(
-                        [
-                            "https://s22.anime-sama.to/s2/",
-                        ]
-                    ),
+        Episode(
+            Languages(
+                vf=Players(),
+                vostfr=Players(
+                    [
+                        "https://s22.anime-sama.to/s2/",
+                    ]
                 ),
-            )
+            ),
         ),
         Path(),
         prefer_languages=["VF", "VOSTFR"],
